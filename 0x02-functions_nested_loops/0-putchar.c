b20433d8710a3611ca38575c425217d78e923479
itayi98 <itayitafirenyika98@gmail.com>
2022-07-05 05:34:48 -0700

    functons_nested_loops

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -6,12 +6,15 @@
  */
 int main(void)
 {
-	_putchar('s');
-	_putchar('c');
-	_putchar('h');
-	_putchar('o');
+	_putchar('H');
 	_putchar('o');
 	_putchar('l');
+	_putchar('b');
+	_putchar('e');
+	_putchar('r');
+	_putchar('t');
+	_putchar('o');	
+	_putchar('n');	
 	_putchar('\n');
 
 	return (0);
